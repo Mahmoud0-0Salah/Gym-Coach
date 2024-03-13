98b1a4dae0b2f89501f16c3b82665aa81d66e8d0
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-03-14 01:52:21 +0200

    Add calculate_angle method

--- Gym-Coach/Gym-Coach.py
+++ Gym-Coach/Gym-Coach.py
@@ -12,6 +12,20 @@ font_thickness = 2
 bg_color = (0, 0, 255)  # Background color (black in this case)
 text_color = (0, 0, 0)
 cap = cv2.VideoCapture(0)
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
+
 while cap.isOpened():
     ret, frame = cap.read()
     frame = cv2.resize(frame, (1300, 700))
